0227cbaf821ae49203f66e12c17f76a88e661a7e
Pieter Delobelle <pieter.delobelle@kuleuven.be>
2024-07-15 15:43:12 +0200

    Dataset loading: Used NLLB mapping for the script suffix

--- tests/test_tiktotok.py
+++ tests/test_tiktotok.py
@@ -35,3 +35,18 @@ def test_get_dataset_iterator_nllb():
     sentence1, sentence2 = next(iterator)
     assert isinstance(sentence1, str)
     assert isinstance(sentence2, str)
+
+def test_get_dataset_iterator_nllb_non_latin():
+    # Test case 3: Testing with allenai/nllb dataset with a non-latin script
+    dataset_name = "allenai/nllb"
+    source_language = "am"
+    target_language = "en"
+    iterator = get_dataset_iterator(dataset_name, source_language, target_language)
+    # we expect tuples (sentence in source language, sentence in target language)``
+    # Check if tuples are returned
+    assert isinstance(next(iterator), tuple)
+
+    # Check if tuples contain two strings
+    sentence1, sentence2 = next(iterator)
+    assert isinstance(sentence1, str)
+    assert isinstance(sentence2, str)
